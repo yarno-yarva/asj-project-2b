9b84c4cfa09c6a695c423e8876e53207996cacc4
yarno <yarno@me.com>
2022-02-07 12:44:15 +0100

    v.1.0

---
+++ src/js/game.js
@@ -0,0 +1,19 @@
+import Character from './domain.js';
+
+class Game {
+  start() {
+    console.log('game started');
+  }
+}
+
+export class GameSavingData {
+  
+}
+
+export function readGameSaving() {
+}
+
+export function writeGameSaving() {
+}
+
+export default Game;
